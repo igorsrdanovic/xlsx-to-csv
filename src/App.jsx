337c857b99a3725fdd054d4edb19ff5f1aa00f6c
Igor Srdanovic <igorsr@gmail.com>
2025-01-04 18:24:03 +0100

    Added GA in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8 px-4">
       <div className="max-w-4xl mx-auto">
+        <Analytics />
         <XLSXConverter />
       </div>
     </div>
